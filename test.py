075d2adb4193e7dd4c6a05d62105664e679f8a7e
Lay-jpg-lab <fawaz.applyingai@gmail.com>
2026-01-12 16:41:10 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,32 @@
+import openpyxl
+import os
+
+# 1️⃣ Excel-Dateien laden
+quelle = openpyxl.load_workbook('../Downloads/Pluxee Excel.xlsx')  # Datei, von der kopiert wird
+ziel = openpyxl.load_workbook('Pluxee_Essenszuschuss_final.xlsx')      # Datei, in die eingefügt wird
+
+# 2️⃣ Arbeitsblätter auswählen
+ws_quelle = quelle.active
+ws_ziel = ziel.active
+
+# 3️⃣ Bereich definieren
+start_zeile_quelle = 4
+start_zeile_ziel = 9
+for x in range(25):
+    start_spalte_quelle = 1  # Spalte A
+    end_spalte_quelle = 10   # Spalte J
+
+    # Zielbereich
+    start_spalte_ziel = 2    # Spalte B
+
+    # 4️⃣ Werte kopieren
+    for i, spalte in enumerate(range(start_spalte_quelle, end_spalte_quelle + 1)):
+        wert = ws_quelle.cell(row=start_zeile_quelle, column=spalte).value
+        ws_ziel.cell(row=start_zeile_ziel, column=start_spalte_ziel + i).value = wert
+
+    # 5️⃣ Ziel-Excel speichern
+    ziel.save('ziel.xlsx')
+    start_zeile_quelle += 1
+    start_zeile_ziel += 1
+
+os.system("start ziel.xlsx")
